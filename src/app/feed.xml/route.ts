1f6843d1ca902a14d2d1d568c3e84936d095b48e
Joey Kim <joey@hanah1.com>
2026-07-17 14:31:13 +0900

    feat: add RSS feed with bilingual titles

---
+++ src/app/feed.xml/route.ts
@@ -0,0 +1,29 @@
+import { sanityClient } from '@/lib/sanity'
+import { allPostsQuery } from '@/lib/queries'
+
+export async function GET() {
+  const posts = await sanityClient.fetch(allPostsQuery)
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://joeyblog.com'
+
+  const items = posts.map((post: any) => `
+    <item>
+      <title><![CDATA[${post.title_en}${post.title_ko ? ' / ' + post.title_ko : ''}]]></title>
+      <link>${baseUrl}/${post.category}/${post.slug.current}</link>
+      <pubDate>${new Date(post.published_at).toUTCString()}</pubDate>
+      <guid>${baseUrl}/${post.category}/${post.slug.current}</guid>
+    </item>`).join('')
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+  <channel>
+    <title>Joey's Blog</title>
+    <link>${baseUrl}</link>
+    <description>Wellness, Christianity, Business</description>
+    ${items}
+  </channel>
+</rss>`
+
+  return new Response(xml, {
+    headers: { 'Content-Type': 'application/xml; charset=utf-8' },
+  })
+}
